6472444e0a36a644a99bd18b0633d1038172b868
aruneer007 <arunvenkatachalam895970@gmail.com>
2024-12-05 14:01:21 +0530

    update

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def decode_img(image):
   img = tf.image.resize(img,[150,150])
   return np.expand_dims(img, axis=0)
 
-path = st.text_input('Enter Image URL to Classify.. ','https://media.springernature.com/full/springer-static/image/art%3A10.1038%2Fs41477-019-0374-3/MediaObjects/41477_2019_374_Figa_HTML.jpg')
+path = st.text_input('Enter Image URL to Classify.. ','https://rewildingeurope.com/wp-content/uploads/2021/11/RS1765_SBA_2011-07-30_070027-low-1202x800.jpg')
 if path is not None:
     content = requests.get(path).content
 
